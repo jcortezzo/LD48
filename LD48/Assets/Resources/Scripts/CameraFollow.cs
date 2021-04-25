3710e2da38bde6c55ebf0aafd9257ae37893c87e
cortejon <cortejon@cs.washington.edu>
2021-04-25 02:06:27 -0700

    working on making the game pretty

--- LD48/Assets/Resources/Scripts/CameraFollow.cs
+++ LD48/Assets/Resources/Scripts/CameraFollow.cs
@@ -9,25 +9,28 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Vector2 offsets;
     [SerializeField] private Vector2 xFollowRange;
     [SerializeField] private Vector2 yFollowRange;
+
+    private float yPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        yPos = target.transform.position.y + offsets.y;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
         Vector2 targetPos = target.transform.position;
-        Vector2 currentPos = this.transform.position;
+        //Vector2 currentPos = this.transform.position;
         float x = 0;
         float y = 0;
 
         x = targetPos.x; //(targetPos.x - currentPos.x > xFollowRange.y) ? targetPos.x : currentPos.x;
         y = targetPos.y; // (targetPos.y - currentPos.y > yFollowRange.y) ? targetPos.y : currentPos.y;
 
-        targetPos = new Vector2(x, y);
-        Vector3 lerpPosition = Vector2.Lerp(this.transform.position, targetPos, lerp) + offsets;
-        this.transform.position = new Vector3(lerpPosition.x, lerpPosition.y, this.transform.position.z);
+        targetPos = new Vector2(x, y);// + offsets;
+        //Vector3 lerpPosition = Vector2.Lerp(this.transform.position, targetPos, lerp) + offsets;
+        //this.transform.position = new Vector3(lerpPosition.x, lerpPosition.y, this.transform.position.z);
+        this.transform.position = new Vector3(targetPos.x + offsets.x, yPos, this.transform.position.z);
     }
 }
