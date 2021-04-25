f15350ea6f274ae98fa88ad4fb843e033c4ff167
cortejon <cortejon@cs.washington.edu>
2021-04-25 02:38:17 -0700

    set up pretty scene. parallax

---
+++ LD48/Assets/Resources/Scripts/Parallax.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length;
+    private Vector2 startPos;
+    [SerializeField] private Camera cam;
+    [SerializeField] private float parallexModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = this.transform.position;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        cam = Camera.main;//GlobalManager.instance.cam;
+    }
+
+    void FixedUpdate()
+    {
+        float temp = cam.transform.position.x * (1 - parallexModifier);
+        float dist = cam.transform.position.x * parallexModifier;
+
+        transform.position = new Vector3(startPos.x + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPos.x + length)
+        {
+            startPos = new Vector2(startPos.x + length, startPos.y);
+        }
+        else if (temp < startPos.x - length)
+        {
+            startPos = new Vector2(startPos.x - length, startPos.y);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
